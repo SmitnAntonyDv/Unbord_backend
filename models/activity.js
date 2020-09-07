c18a72e552f5ba85c8535cc3604fcd64f465a84d
SmitnAntonyDv <anton.smit@hotmail.com>
2020-09-07 20:33:35 +0200

    development branch init

--- models/activity.js
+++ models/activity.js
@@ -6,7 +6,7 @@ module.exports = (sequelize, DataTypes) => {
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
-     */
+      branch */
     static associate(models) {
       // define association here
     }
