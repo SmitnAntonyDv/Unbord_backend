0536ea70224b7db716eb435443a40d084006b0bb
Andre2610 <dre2610@gmail.com>
2020-09-09 13:29:03 +0200

    server running on port 5000

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 require("dotenv").config();
 const express = require("express");
-// const authRouter = require("./router/auth");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
@@ -8,6 +7,5 @@ const jsonParser = express.json();
 
 app.use(cors());
 app.use(jsonParser);
-// app.use("/auth", authRouter);
 
 app.listen(port, () => console.log("Listening to port", port));
