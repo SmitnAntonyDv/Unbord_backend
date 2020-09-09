eaa6d489e96930a8469025ea96f0e12795aa00d7
Andre2610 <dre2610@gmail.com>
2020-09-09 13:23:21 +0200

    installed express and created express server

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const express = require("express");
-const authRouter = require("./router/auth");
+// const authRouter = require("./router/auth");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
@@ -8,6 +8,6 @@ const jsonParser = express.json();
 
 app.use(cors());
 app.use(jsonParser);
-app.use("/auth", authRouter);
+// app.use("/auth", authRouter);
 
 app.listen(port, () => console.log("Listening to port", port));
