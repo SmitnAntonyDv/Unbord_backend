26287dcced81331123e7b6e5f908955acfdf1607
Andre2610 <dre2610@gmail.com>
2020-09-09 15:28:24 +0200

    created email token for email verification link

--- auth/jwt.js
+++ auth/jwt.js
@@ -3,6 +3,18 @@ const jwt = require("jsonwebtoken");
 const secret =
   process.env.JWT_SECRET || "e9rp^&^*&@9sejg)DSUA)jpfds8394jdsfn,m";
 
+const emailSecret =
+  process.env.EMAIL_SECRET || "h6vs^&^*&@9sejg)DSUA)jpfds1234jdsfn,m";
+
+function emailToken(data) {
+  return jwt.sign(data, emailSecret, { expiresIn: "24h" });
+}
+
+function validatingEmail(emailToken) {
+  console.log("my email secret", emailSecret);
+  return jwt.verify(emailToken, emailSecret);
+}
+
 function toJWT(data) {
   return jwt.sign(data, secret, { expiresIn: "2h" });
 }
