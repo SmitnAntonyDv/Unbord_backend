5b2f1e38065c7968f9e7b563139623f0db5bb89f
Andre2610 <dre2610@gmail.com>
2020-09-09 15:27:13 +0200

    created signup endpoint

--- router/auth.js
+++ router/auth.js
@@ -1,10 +1,21 @@
 const { Router } = require("express");
+const { toJWT, emailToken, validatingEmail } = require("../auth/jwt");
+const authMiddleware = require("../auth/middleware");
+const bcrypt = require("bcrypt");
+const {
+  SALT_ROUNDS,
+  BACKEND_API,
+  API_URL,
+  AUTH_USER,
+  AUTH_PASS,
+} = require("../config/constants");
+const nodemailer = require("nodemailer");
 const User = require("../models").user;
 
 const router = new Router();
 
+// login logic
 router.post("/login", async (req, res, next) => {
-  // login logic
   try {
     const { email, password } = req.body;
     if (!email || !password || email === " " || password === " ") {
@@ -32,5 +43,77 @@ router.post("/login", async (req, res, next) => {
     res.send({ token, ...user.dataValues });
   } catch (e) {
     next(e);
+  }
+});
+
+// CREATE a new user
+router.post("/signup", async (req, res) => {
+  const { email, password, firstName, lastName, title, about } = req.body.data;
+  if (!email || !password || !firstName || !lastName) {
+    return res.status(400).send("Please provide an email, password and a name");
+  }
+
+  try {
+    const newUser = await User.create({
+      firstName,
+      lastName,
+      email,
+      password: bcrypt.hashSync(password, SALT_ROUNDS),
+      title,
+      about,
+      verified: false,
+    });
+
+    const eToken = emailToken({ id: newUser.id });
+    // created encrypted email link
+    const url = `${BACKEND_API}/auth/confirmation/${eToken}`;
+
+    const transporter = nodemailer.createTransport({
+      service: "outlook",
+      auth: {
+        user: `${AUTH_USER}`,
+        pass: `${AUTH_PASS}`,
+      },
+    });
+
+    const confirmationEmailTemplate = {
+      from: `${AUTH_USER}`, // sender address
+      to: `${email}`, // list of receivers
+      subject: `Hello, ${firstName}`, // Subject line
+      text: `Thank you for registering an account with space travel agency. 
+        Please confirm your email by clicking the following link:${url}`, // plain text body
+      html: `      
+        <div 
+          style=
+          padding: 5px 5px 5px 5px;
+          line-spacing: 2rem;
+          border="0";
+          width="600";
+        >
+          <h2>Dear ${firstName}, thank you for joining the Traveler's Diary community!</h2>
+          <h3>IMPORTANT:</h3>
+          <p>Please confirm your email by clicking the following link:<a href=${url}>${url}</a></p>
+        </div>
+  
+        `, // html body
+    };
+
+    transporter.sendMail(confirmationEmailTemplate, function (err, data) {
+      if (err) {
+        console.log("Error Occurs", err);
+      } else {
+        console.log("Email sent!!!");
+      }
+    });
+
+    res.status(201).json("Success");
+  } catch (error) {
+    if (error.name === "SequelizeUniqueConstraintError") {
+      return res
+        .status(400)
+        .send({ message: "There is an existing account with this email" });
+    }
+
+    return res.status(400).send({ message: "Something went wrong, sorry" });
   }
 });
