826f9418bb9678e200544eb1b9e36236c26a1ef8
Andre2610 <dre2610@gmail.com>
2020-09-11 16:39:54 +0200

    created /auth router in index.js server

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const authRouter = require("./router/auth.js");
 const port = process.env.PORT || 5000;
 const jsonParser = express.json();
 
 app.use(cors());
 app.use(jsonParser);
+
+app.use("/auth", authRouter);
 
 app.listen(port, () => console.log("Listening to port", port));
